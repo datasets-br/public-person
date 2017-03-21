a825d271d9f668d5a364c6e6fca2981c1f485d38
Peter <ppkrauss@gmail.com>
2017-03-21 16:52:05 -0300

    LEMBRETES

--- src/tse.sql
+++ src/tse.sql
@@ -19,24 +19,41 @@ CREATE FUNCTION lib.get_csvfile(
 $f$ immutable language PLpythonU;
 -- for lib.get_csvline() see http://stackoverflow.com/a/42929480/287948
 
-CREATE or replace FUNCTION lib.csv_scan_sources(
+CREATE FUNCTION lib.csv_scan_sources(
    filepath text  -- something as '/tmp/test/*.txt'
-) RETURNS text[]  AS $f$
+) RETURNS setof text  AS $f$
   import glob
-  # import numpy as np
-  #np.asarray( sorted(glob.glob(filepath)) )
-  dataset_list = ';'.join(sorted(glob.glob(filepath)))
-  dataset_array = []
-  for item in dataset_list.split(';'): # comma, or other
-     dataset_array.append(item)
-  dataset_array
+  return sorted(glob.glob(filepath))
 $f$ immutable language PLpythonU;
+
+CREATE or replace FUNCTION lib.set_serial_tomax(
+  tname text, id_name text
+) RETURNS void AS $f$
+BEGIN
+      EXECUTE format(
+        'SELECT setval(pg_get_serial_sequence(%L, %L), coalesce(max(%s),0) + 1, false) FROM %s;',
+        tname, id_name, id_name, tname
+      );
+END
+$f$ language PLpgSQL;
+
+CREATE FUNCTION lib.array_select_idx(
+   text[],   -- input array
+   idxs int[]  -- the indexes to be selected
+) RETURNS text[]  AS $f$
+   SELECT array_agg(x)
+   FROM (
+     SELECT $1[x] AS x FROM unnest($2) t(x)
+   ) t2;
+$f$ immutable language SQL;
 
 
 -- -- -- -- -- -- --
 
-select x from unnest( lib.csv_scan_sources('/tmp/TSE2/*.txt') ) t(x);
-
+CREATE VIEW pubperson.vw_tmp_sources AS
+  SELECT row_number() OVER () as id, regexp_matches(item, '(\d\d\d\d)_(..).txt$') as ano_uf, *
+  FROM lib.csv_scan_sources('/tmp/TSE/consulta_cand*.*') t(item)
+  ORDER BY item;
 
 CREATE TABLE pubperson.source (
  id bigserial NOT NULL PRIMARY KEY,
@@ -46,21 +63,17 @@ CREATE TABLE pubperson.source (
  UNIQUE(label)
 );
 
-CREATE TABLE pubperson.br_tse_person (
- id bigserial NOT NULL PRIMARY KEY,
- source_id bigint REFERENCES pubperson.source(id),
- fullname text,
- vatid bigint,
- info JSONb
-);
-CREATE INDEX idx_tse_fullname ON pubperson.br_tse_person (fullname);
-
-UPDATE pubperson.br_tse_person
-SET source_id = src.id
-FROM pubperson.source src
-WHERE src.info->'id' = br_tse_person.info->'fonte_id'
-; -- wait ...
-
+INSERT INTO pubperson.source(id,label,info)
+  SELECT id, 
+         'br;'|| lower(ano_uf[2]) ||':tse:candidatos:'||ano_uf[1] AS label, 
+         jsonb_build_object(
+             'year',ano_uf[1],          'country','BR', 
+             'state',ano_uf[2],         'file',item,
+             'isBasedOnUrl','http://www.tse.jus.br/eleicoes/estatisticas/repositorio-de-dados-eleitorais'
+         )
+   FROM pubperson.vw_tmp_sources
+;
+SELECT lib.set_serial_tomax('pubperson.source','id');
 
 
 CREATE TABLE pubperson.person (
@@ -71,6 +84,26 @@ CREATE TABLE pubperson.person (
  info JSONb
 );
 CREATE INDEX idx_fullname ON pubperson.person (fullname);
+
+-- select id,label, (info->>'year')::int as year, s.info->>'file' as filename 
+-- from pubperson.source;
+
+-- nao funciona info->>'year' < to_jsonb(2004)
+
+-- process "Killed"! reduzir por blocos range de id.
+
+-- good:
+--SELECT lib.array_select_idx(c,array[2,5,10,26,27])  
+--FROM lib.get_csvfile('/tmp/TSE/consulta_cand_1994_RS.txt', ';') t(c), pubperson.source s;
+
+-- working: to insert into pubperson.person
+--SELECT  lib.array_select_idx(c,array[2,5,10,26,27])
+--FROM pubperson.source s, LATERAL lib.get_csvfile(s.info->>'file') g(c)
+--WHERE s.id>100 AND s.id<110;
+
+SELECT  s.label, lib.array_select_idx(c,array[2,5,10,26,27])
+FROM pubperson.source s, LATERAL lib.get_csvfile(s.info->>'file',';') g(c)
+WHERE s.id>0 AND s.id<10;
 
 
 
